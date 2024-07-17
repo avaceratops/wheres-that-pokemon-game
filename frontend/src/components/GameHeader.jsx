4e46c186d07b9b219e70f8dcb5a80c167c2650a6
ava <148492613+avaceratops@users.noreply.github.com>
2024-07-17 13:31:56 +0100

    Add GameHeader component
    Renders a header which displays the page title, sprites of each Pokemon
    the user must find, and a timer which denotes the total play time.

---
+++ frontend/src/components/GameHeader.jsx
@@ -0,0 +1,25 @@
+import { Duration } from 'luxon';
+import PokemonSprite from './PokemonSprite';
+
+export default function GameHeader({ solved, playTime }) {
+  return (
+    <header
+      className="xs:flex-row xs:justify-between fixed top-0 z-20 flex w-full flex-col items-center
+        gap-2 border-b border-gray-200 bg-white/90 px-4 py-2 backdrop-blur sm:gap-20"
+    >
+      <h1 className="hidden text-2xl font-bold leading-none tracking-tight md:block">
+        where&apos;s that pokémon?
+      </h1>
+
+      <section className="flex gap-6">
+        <PokemonSprite name="Togepi" solved={solved} />
+        <PokemonSprite name="Omanyte" solved={solved} />
+        <PokemonSprite name="Bonsly" solved={solved} />
+      </section>
+
+      <section className="text-right font-mono lg:w-60">
+        <p>{Duration.fromObject({ second: playTime }).toFormat('hh:mm:ss')}</p>
+      </section>
+    </header>
+  );
+}
