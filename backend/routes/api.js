1171a4228e552bab6501fd904a2cbde4f08b1810
ava <148492613+avaceratops@users.noreply.github.com>
2024-07-19 13:49:42 +0100

    Update API route with sessions/leaderboard

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,6 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 const coordController = require('../controllers/coordController');
+const scoreController = require('../controllers/scoreController');
+
+router.post('/sessions', scoreController.add_session);
+router.post('/session/:sessionId', scoreController.update_session);
+
+router.get('/leaderboard', scoreController.get_scores);
+router.post('/leaderboard', scoreController.add_score);
 
 router.post('/coord/:name', coordController.check_coord);
 
