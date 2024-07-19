226acb53f1afe85c8ad9582c7495e20b55e001e2
ava <148492613+avaceratops@users.noreply.github.com>
2024-07-19 13:46:46 +0100

    Update App.jsx with leaderboard functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,65 @@
 import { useEffect, useState } from 'react';
 import api from './utils/api';
-import { formatTime } from './utils/timer';
 import ClickableImage from './components/ClickableImage';
 import GameHeader from './components/GameHeader';
-import Modal from './components/Modal';
+import Leaderboard from './components/Leaderboard';
 import TaggingCircle from './components/TaggingCircle';
 import TaggingOption from './components/TaggingOption';
 import TaggingSelect from './components/TaggingSelect';
+import WinMessage from './components/WinMessage';
 
 export default function App() {
   const [tagCoords, setTagCoords] = useState(null);
   const [solved, setSolved] = useState({});
   const [playTime, setPlayTime] = useState(0);
+  const [startTime, setStartTime] = useState(null);
   const [sessionId, setSessionId] = useState(null);
+  const [scores, setScores] = useState([]);
 
   const solvedCount = Object.keys(solved).length;
 
   // start client-side timer
   useEffect(() => {
-    const id = setInterval(() => {
-      if (solvedCount < 3) {
-        setPlayTime((prevTime) => prevTime + 1);
-      } else {
-        clearInterval(id);
-      }
-    }, 1000);
+    let id;
+    if (startTime) {
+      id = setInterval(() => {
+        setPlayTime(Date.now() - startTime);
+      }, 10);
+    }
     return () => clearInterval(id);
-  }, [solvedCount]);
+  }, [startTime]);
+
+  // stop server-side timer
+  useEffect(() => {
+    async function endSession() {
+      try {
+        const response = await api.post(`/session/${sessionId}`);
+        if (response.status === 200 && response.data.session) {
+          const { startTime, endTime } = response.data.session;
+          setPlayTime(endTime - startTime);
+          setStartTime(0);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    async function getScores() {
+      try {
+        const response = await api.get('/leaderboard');
+        if (response.status === 200 && response.data) {
+          setScores(response.data.scores);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    if (solvedCount === 3) {
+      endSession();
+      getScores();
+    }
+  }, [sessionId, solvedCount]);
 
   // get session id from server
   useEffect(() => {
@@ -35,6 +68,7 @@ export default function App() {
         const response = await api.post('/sessions');
         if (response.status === 200) {
           setSessionId(response.data.sessionId);
+          setStartTime(response.data.startTime);
         }
       } catch (err) {
         console.error(err);
@@ -76,37 +110,61 @@ export default function App() {
     }
   };
 
+  const updateScores = (name) => {
+    const updatedScores = [...scores, { name, score: playTime }];
+
+    updatedScores.sort((a, b) => a.score - b.score);
+    // only keep the top 50 entries
+    if (updatedScores.length > 50) {
+      updatedScores.pop();
+    }
+    setScores(updatedScores);
+  };
+
   const resetGame = () => {
+    setSessionId(null);
     setSolved({});
-    setPlayTime(0);
   };
 
   return (
     <>
       <GameHeader solved={solved} playTime={playTime} />
-      <Modal
-        isOpen={solvedCount === 3}
-        title="You beat the game!"
-        desc={`It took you ${formatTime(playTime)}.`}
-        buttonText="Restart"
-        onClick={resetGame}
-      />
 
       {/* spacer for fixed header */}
       <div className="mb-24 xs:mb-16" />
 
-      <main className="m-auto">
-        <section className="relative m-2">
-          <ClickableImage src={'/montage.jpg'} alt="Clickable Pokemon montage" onClick={tagImage} />
+      {solvedCount === 3 && (
+        <main className="p-4">
+          <WinMessage playTime={playTime} resetGame={resetGame} />
+          <hr className="my-4" />
+          <Leaderboard
+            playTime={playTime}
+            sessionId={sessionId}
+            numEntries={50}
+            scores={scores}
+            updateScores={updateScores}
+          />
+        </main>
+      )}
 
-          <TaggingCircle coords={tagCoords} />
-          <TaggingSelect coords={tagCoords}>
-            <TaggingOption name="Togepi" solved={solved} onClick={submitCoords} />
-            <TaggingOption name="Omanyte" solved={solved} onClick={submitCoords} />
-            <TaggingOption name="Bonsly" solved={solved} onClick={submitCoords} />
-          </TaggingSelect>
-        </section>
-      </main>
+      {solvedCount < 3 && (
+        <main className="m-auto p-4">
+          <section className="relative m-2">
+            <ClickableImage
+              src={'/montage.jpg'}
+              alt="Clickable Pokemon montage"
+              onClick={tagImage}
+            />
+
+            <TaggingCircle coords={tagCoords} />
+            <TaggingSelect coords={tagCoords}>
+              <TaggingOption name="Togepi" solved={solved} onClick={submitCoords} />
+              <TaggingOption name="Omanyte" solved={solved} onClick={submitCoords} />
+              <TaggingOption name="Bonsly" solved={solved} onClick={submitCoords} />
+            </TaggingSelect>
+          </section>
+        </main>
+      )}
     </>
   );
 }
